f806b5fe9bc707de4fa40439e4e2e171d3179132
my form <ellaemezieobi456@gmail.com>
2021-11-19 14:32:46 +0100

    clear commented code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,3 @@
-// const html = document.getElementById('html');
-// const body = document.getElementById('body');
-// const burger = document.getElementById('burger');
-// const navlink = document.getElementById('navlink');
-// const main = document.querySelector('main');
-// window.addEventListener("scroll", () => {
-//     let y = html.scrollTop;
-//     if(y>10){
-//         nav.style.borderBottom = "1px solid rgb(229, 229, 229) ";
-//     }else{
-//         nav.style.borderBottom = "none";
-//     }
-// })
-
-// burger.addEventListener('click', () => {
-//    navlink.classList.toggle('navactive');
-// })
-
-// main.addEventListener('click', () =>{
-//     if(navlink.classList.contains('navactive')){
-//         navlink.classList.remove("navactive");
-//     }
-// })
-
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
