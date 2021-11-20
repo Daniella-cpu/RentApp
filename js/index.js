83d87025613735576251c98ff3e935e7269ea8f8
my form <ellaemezieobi456@gmail.com>
2021-11-20 13:26:19 +0100

    scroll event

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+let nav = document.getElementById('nav');
+
+window.addEventListener("scroll", ()  => {
+    let y = window.scrollY;
+    if(y>20){
+        nav.style.borderBottom = "1px rgb(229, 229, 229) solid"
+    }else{
+        nav.style.borderBottom = "none";
+    }
+});
 
 hamburger.addEventListener("click", mobileMenu);
 
